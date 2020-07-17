034aa573a812b83d9d1225329451a800f4f4df6f
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2020-07-17 17:36:50 +0200

    Boxplot for all conditions in isopods

--- scripts/replicate_analysis.py
+++ scripts/replicate_analysis.py
@@ -5,7 +5,8 @@ from plot_module import *
 
 os.chdir("../DataEmpirical")
 exp_dirs = sorted(
-    set(["_".join(i.split("_")[:-1]) for i in os.listdir("Experiments") if ("Replicates" in i) and ("Kappa" in i)]))
+    set(["_".join(i.split("_")[:-1]) for i in os.listdir("Experiments") if
+         ("Replicates" in i) and ("Isopods" in i) and ("Sample12" in i)]))
 os.makedirs("Analysis", exist_ok=True)
 file_format = "pdf"
 
@@ -34,13 +35,14 @@ for exp in exp_dirs:
         axis_dict, err_dict, taxa_dict = dict(), dict(), dict()
 
         nb_values = -1
-        for index, folder in enumerate(folder_list):
+        for index, folder in enumerate(sorted(folder_list)):
             name = folder.split("_")[-1]
             with open(folder + "/" + tree_name, 'r') as tree_file:
                 tree = Tree(remove_units(tree_file.readline()), format=1)
 
             values = np.array([to_float(getattr(n, feature)) for n in tree.traverse() if feature in n.features])
-            taxa_dict[name] = np.array([to_float(getattr(n, feature)) for n in tree if feature in n.features])
+            taxa_dict["Replicate {0}".format(index + 1)] = np.array(
+                [to_float(getattr(n, feature)) for n in tree if feature in n.features])
             if nb_values == -1:
                 nb_values = len(values)
             if len(values) != nb_values:
@@ -54,13 +56,50 @@ for exp in exp_dirs:
             err_dict[name] = np.vstack((np.abs(values - min_values), np.abs(max_values - values)))
 
         if "Isopods" in exp:
+            label = label_transform(tree_name.split(".")[-2])
+            label = "Log " + label[0].lower() + label[1:]
+            dict_trait = {"eco": "Habitat", "pig": "Pigmentation", "eye": "Ocular structure"}
+            dict_trait_def = {"eco": {"epi": "Surface", "hypo": "Underground"},
+                              "pig": {"D": "Depigmented", "DP": "Partially depigmented", "P": "Pigmented"},
+                              "eye": {"A": "Anophthalmia", "M": "Microphtalmia", "O": "Ocular"}}
             df = pd.DataFrame.from_dict(taxa_dict)
-            for x in ["eco", "pig", "eye"]:
-                df[x] = pd.Series([dataset[dataset['code'] == i].iloc[0][x] for i in tree.get_leaf_names()])
-                df.boxplot(column=list(taxa_dict.keys()), by=x, figsize=(len(taxa_dict), 2), layout=(1, len(taxa_dict)),
-                           notch=True)
+            for trait, trait_name in dict_trait.items():
+                df[trait_name] = pd.Series([dict_trait_def[trait][dataset[dataset['code'] == i].iloc[0][trait]] for i in
+                                            tree.get_leaf_names()])
+                axes = df.boxplot(column=list(taxa_dict.keys()), by=trait_name, figsize=(len(taxa_dict), 2),
+                                  layout=(1, len(taxa_dict)), rot=90, fontsize=6)
+                axes[0].set_ylabel(label, fontsize=6)
+                for ax in axes:
+                    ax.set_xlabel("")
+                    ax.set_title(ax.get_title(), {'fontsize': 8})
+                plt.suptitle("")
                 plt.tight_layout()
-                plt.savefig("Analysis/{0}/{1}.{2}.{3}".format(exp, tree_name, x, file_format), format=file_format)
+                plt.savefig("Analysis/{0}/{1}.{2}.{3}".format(exp, tree_name, trait, file_format), format=file_format)
                 plt.clf()
+            df["cu"] = pd.Series([dataset[dataset['code'] == i].iloc[0]["cu"] for i in tree.get_leaf_names()])
+            df["morphosp"] = pd.Series(
+                [dataset[dataset['code'] == i].iloc[0]["morphosp"] for i in tree.get_leaf_names()])
+            df["station"] = pd.Series([dataset[dataset['code'] == i].iloc[0]["station"] for i in tree.get_leaf_names()])
+            df["code"] = pd.Series(tree.get_leaf_names())
+            df.to_csv("Analysis/" + exp + "_" + tree_name.replace(".nhx", '.tsv'), index=False, sep="\t")
+
+            frames = []
+            for k in taxa_dict.keys():
+                d = {label: df[k], 'code': df["code"], "rep": [k] * len(df[k])}
+                for trait_name in dict_trait.values():
+                    d[trait_name] = df[trait_name]
+                frames.append(pd.DataFrame(d))
+            merged = pd.concat(frames)
+            for trait, trait_name in dict_trait.items():
+                ax = merged.boxplot(column=[label], by=trait_name, fontsize=14, notch=True)
+                ax.set_ylabel(label, fontsize=14)
+                ax.set_xlabel("")
+                ax.set_title(trait_name, fontsize=14)
+                plt.suptitle("")
+                plt.tight_layout()
+                plt.savefig("Analysis/{0}/{1}.{2}.merged.{3}".format(exp, tree_name, trait, file_format),
+                            format=file_format)
+                plt.clf()
+            merged.to_csv("Analysis/" + exp + "_merged_" + tree_name.replace(".nhx", '.tsv'), index=False, sep="\t")
         plot_correlation("Analysis/" + exp + "/" + tree_name.replace(".nhx", '.' + file_format), axis_dict, err_dict,
-                         [], global_xy=True)
+                         global_min_max=True)
